ba4bfa80041ee64e2da732eadadbb851a029a4c1
Lucas Stefano <lucassxs.sp@gmail.com>
2022-08-07 18:32:35 -0300

    add bmi eq

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 altura = float(input("Digite a sua altura em cm:  "))
 peso = float(input("Digite o seu peso em kg:  "))
+
+
+## Adicionando o cálculo do IMC
+IMC = altura / (peso/100)**2
+
+print(f"Seu IMC é {IMC}")
