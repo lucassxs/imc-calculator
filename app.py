c4ff6cb91e9ced0ed6850c5035f598ad3d16fdb7
Lucas Stefano <lucassxs.sp@gmail.com>
2022-08-07 18:38:32 -0300

    add bmi table

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ peso = float(input("Digite o seu peso em kg:  "))
 IMC = altura / (peso/100)**2
 
 print(f"Seu IMC é {IMC}")
+
+## Tabela de IMC
+
+if IMC <= 18.4:
+    print("Abaixo do peso")
+elif IMC <= 24.9:
+    print("Peso Normal")
+elif IMC <= 29.9:
+    print("Sobrepeso") 
+elif IMC <= 34.9:
+    print("Obesidade Grau I")
+elif IMC <= 39.9:
+    print("Obesidade Grau II")   
+else:
+    print("Obsesidade Grau III ou Mórbida")
